95cd168a589dbab9a815eda97885ab5a31ebb610
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-11-08 09:13:12 +0500

    789i2

--- src/app/Pages/system/profile/profile.ts
+++ src/app/Pages/system/profile/profile.ts
@@ -62,7 +62,7 @@ export class Profile implements OnInit {
   // Oylik ma'lumotlar (darslar soni va uy vazifa berilgan darslar soni)
   monthDetails: { [key: string]: { totalLessons: number, homeworkLessons: number } } = {
     "2025-10": { totalLessons: 10, homeworkLessons: 10 },
-    "2025-11": { totalLessons: 2, homeworkLessons: 2 }
+    "2025-11": { totalLessons: 3, homeworkLessons: 3 }
   };
 
   currentMonth = '2025-11'; // Joriy oy
