9f3d9b9933099e8e09e872964ded32b69e484f7b
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-12-16 09:21:25 +0500

    3rfedfefref23e

--- src/app/Pages/system/profile/profile.ts
+++ src/app/Pages/system/profile/profile.ts
@@ -62,7 +62,7 @@ export class Profile implements OnInit {
   monthDetails: { [key: string]: { totalLessons: number, homeworkLessons: number } } = {
     "2025-10": { totalLessons: 10, homeworkLessons: 10 },
     "2025-11": { totalLessons: 13, homeworkLessons: 13 },
-    "2025-12": { totalLessons: 6, homeworkLessons: 6 }
+    "2025-12": { totalLessons: 8, homeworkLessons: 8 }
   };
 
   currentMonth = '2025-12'; // Joriy oy
